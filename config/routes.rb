981493248a2db5521ed61bf36cc8c23be697f36a
Niusha H <niusha.hadziomerovic@gmail.com>
2021-12-07 16:10:51 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  # :edit in the line above is a shortcut for: get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # :update in the line above is a shortcut for: patch 'articles/:id', to: 'articles#update'
+
 end
